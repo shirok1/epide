f7f2637898b57806e7b1352ecd47da21512e76f7
shirok1 <tomatroom@qq.com>
2020-08-16 13:39:40 +0800

    Fix build error in TextChangedEventArgs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
